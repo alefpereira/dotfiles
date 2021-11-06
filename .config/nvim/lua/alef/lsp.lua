3c9b4f2d6199125d029e034a7aeb6e5f023efd43
Alef Pereira <ealefpereira@gmail.com>
2021-11-05 20:29:40 -0400

    Fix lua language server to understand nvim modules

--- .config/nvim/lua/alef/lsp.lua
+++ .config/nvim/lua/alef/lsp.lua
@@ -73,12 +73,33 @@ configs.ls_emmet = {
   };
 }
 
--- lua
+-- lua sumneko language server config (Copy of ThePrimeagen)
 local sumneko_root_path = vim.env.HOME .. "/Softwares/lua-language-server"
 local sumneko_binary = sumneko_root_path .. "/bin/Linux/lua-language-server"
 
 local sumneko_config = {
-    cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" }
+    cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+    settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Setup your lua path
+				path = vim.split(package.path, ";"),
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
+			},
+		},
+	},
 }
 
 -- table of servers to setup {"<server-name>", <opts>}
