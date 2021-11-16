bd4ea142268573296b1e6d82ae3244a8974e1207
Alef Pereira <ealefpereira@gmail.com>
2021-11-16 04:27:57 -0400

    Fix test if venv exists always returning true

--- .config/fish/functions/nvimvenv.fish
+++ .config/fish/functions/nvimvenv.fish
@@ -1,7 +1,7 @@
 # Autosource virtualenv; Workarround for nvim
 function nvimvenv
-  if test -e $VIRTUAL_ENV; and test -f $VIRTUAL_ENV/bin/activate.fish
-    source $VIRTUAL_ENV/bin/activate.fish
+  if test -e "$VIRTUAL_ENV"; and test -f "$VIRTUAL_ENV/bin/activate.fish"
+    source "$VIRTUAL_ENV/bin/activate.fish"
     /usr/bin/nvim $argv # When using aliases its safer to use absolute path
     deactivate
   else
