6b913d780d3f50be56c55fc5e9d2c7ca1b815fba
Alef Pereira <ealefpereira@gmail.com>
2022-01-12 16:14:55 -0400

    Change lualine separator

--- .config/nvim/lua/alef/lualine.lua
+++ .config/nvim/lua/alef/lualine.lua
@@ -2,7 +2,7 @@ require'lualine'.setup{
   options = {
     icons_enabled = true,
     theme = 'onedark',
-    component_separators = { left = '', right = '|'},
+    component_separators = { left = '', right = '︱'},
     section_separators = { left = '', right = ''},
   },
   sections = {
