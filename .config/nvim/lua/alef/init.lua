8dc9b40b1cffe8f2462bbefd25e9170945792b24
Alef Pereira <ealefpereira@gmail.com>
2022-01-12 14:03:15 -0400

    Create a new config file for lualine

--- .config/nvim/lua/alef/init.lua
+++ .config/nvim/lua/alef/init.lua
@@ -2,25 +2,4 @@ require("alef.telescope")
 require("alef.lsp")
 require("alef.treesitter")
 require("alef.git")
-
-
-require'lualine'.setup{
-  options = {
-    icons_enabled = true,
-    theme = 'onedark',
-    component_separators = { left = '', right = '|'},
-    section_separators = { left = '', right = ''},
-  },
-  sections = {
-    -- lualine_b = {{'branch', icon=''}},
-    -- lualine_b = {{'FugitiveHead', icon=''}},
-    -- lualine_b = {{'b:gitsigns_head', icon=''}},
-    lualine_c = {
-      {
-        'filename',
-        file_status = true,  -- displays file status (readonly status, modified status)
-        path = 1,            -- 0 = just filename, 1 = relative path, 2 = absolute path
-      }
-    }
-  }
-}
+require("alef.lualine")
