419785ea8d5e7a9f87f52cffd8505264043a6a63
Alef Pereira <ealefpereira@gmail.com>
2022-01-12 22:42:08 -0400

    Fix cmp snippets

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -35,12 +35,14 @@ Plug 'lewis6991/gitsigns.nvim'
 Plug 'neovim/nvim-lspconfig'
 
 " Autocompletion
+Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 "Plug 'hrsh7th/cmp-path'
 "Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
 Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'rafamadriz/friendly-snippets'
 
 " Other related to lsp (ui references, outlines)
 "Plug 'glepnir/lspsaga.nvim'
--- .config/nvim/lua/alef/lsp.lua
+++ .config/nvim/lua/alef/lsp.lua
@@ -3,13 +3,19 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.pumheight = 15
 
 -- Setup nvim-cmp.
+local check_backspace = function()
+  local col = vim.fn.col "." - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+local luasnip = require('luasnip')
 local cmp = require'cmp'
+require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
   mapping = {
@@ -21,6 +27,20 @@ cmp.setup({
     ['<C-e>'] = cmp.config.disable,
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<NL>'] = cmp.mapping.confirm({ select = false }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then cmp.select_next_item()
+      elseif luasnip.expandable() then  luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+      elseif check_backspace() then fallback()
+      else fallback()
+      end
+    end, { "i", "s"}),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+      else fallback()
+      end
+    end, { "i", "s"}),
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
