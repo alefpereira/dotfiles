535db8be2db8bdc8c27d9f8257d9d6a35c6cc5a7
Alef Pereira <ealefpereira@gmail.com>
2022-01-25 11:31:10 -0400

    Fix colors, most settings and many mappings
    Still need to fix telescope, lsp and some specific mappings.
    Also need to fix some specific settings.

--- .config/nvim/plugin/colors.lua
+++ .config/nvim/plugin/colors.lua
@@ -1,25 +1,24 @@
---[[
-fun! ColorMyPencils()
-    "" Colors
-    " Terminal Color Scheme
-    "let g:gruvbox_contrast_dark='hard'
-    "colorscheme gruvbox
-    "For "codedark" also set netrwMarkFile to fix netrw marked files
-    colorscheme codedark
-    highlight netrwMarkFile ctermbg=237
+function ColorMyPencils()
+  -- Colors
+  -- Terminal Color Scheme
+  --vim.g.gruvbox_contrast_dark='hard'
+  --vim.cmd('colorscheme gruvbox')
+  --For "codedark" also set netrwMarkFile to fix netrw marked files
+  vim.cmd('colorscheme codedark')
+  -- TODO: Use vim.highlight.create to setup highlights
+  vim.cmd('highlight netrwMarkFile ctermbg=237')
 
-    " Column lines color
-    highlight ColorColumn ctermbg=238
+  -- Column lines color
+  vim.cmd('highlight ColorColumn ctermbg=238')
 
-    " CursorLineLn white foreground and grey background
-    highlight CursorLineNr ctermbg=236 ctermfg=248
+  -- CursorLineLn white foreground and grey background
+  vim.cmd('highlight CursorLineNr ctermbg=236 ctermfg=248')
 
-    " GitSigns and LineBar
-    highlight! link SignColumn LineNr
-    highlight GitSignsAdd      ctermfg=2 ctermbg=234
-    highlight GitSignsChange   ctermfg=4 ctermbg=234
-    highlight GitSignsDelete  ctermfg=9 ctermbg=234
-endfun
+  -- GitSigns and LineBar
+  vim.cmd('highlight! link SignColumn LineNr')
+  vim.cmd('highlight GitSignsAdd      ctermfg=2 ctermbg=234')
+  vim.cmd('highlight GitSignsChange   ctermfg=4 ctermbg=234')
+  vim.cmd('highlight GitSignsDelete   ctermfg=9 ctermbg=234')
+end
 
-call ColorMyPencils()
---]]
+ColorMyPencils()
--- .config/nvim/plugin/settings.lua
+++ .config/nvim/plugin/settings.lua
@@ -1,75 +1,5 @@
---[[
-"" Minimal (but really valuable) settings
-" Show line numbers
-set number
-" Enable syntax
-" (enable/uncomment here can cause weird behaviour in colors as vim-plug
-" also enables it)
-"syntax on
-" Enable filetype detection, pluins and indentation
-filetype plugin indent on
-" Always display the status bar
-set laststatus=2
-" Always show the command typed in the status line
-set showcmd
-" Always show cursor position
-set ruler
-" Highlight search results
-set hlsearch
-" Search as you type
-set incsearch
-" Display command line’s tab complete options as a menu
-set wildmenu
-" Set screen to scroll following cursor offset
-set scrolloff=8
-
-" Personal choices
-" Set lines not to wrap
-set nowrap
-" Show line relative numbers instead of usual sequential numbers
-set relativenumber
-" show existing tab with 4 spaces width
-set tabstop=4
-set softtabstop=-1
-" when indenting with '>', use 4 spaces width
-set shiftwidth=0
-" On pressing tab, insert 4 spaces
-set expandtab
-" Smartcase
-set ignorecase
-set smartcase
-
-" More customization
-" Highlight current line
-set cursorline
-" Highlight colums
-set colorcolumn=80,100
-" Hide native mode indicator
-set noshowmode
-" Set update time so vim updates faster
-set updatetime=50
-" Setup persistent undo history
-set noswapfile
-set nobackup
-set undodir=~/.vim/undodir
-set undofile
-set hidden
-" Hide Pattern not found message from completion
-set shortmess+=c
-
+--[[FIXME
 " ====== Other settings ======
-"" Leader map
-" Set leader to space
-let mapleader = " "
-
-"" Let vim to highlight embeded code: Lua, Python
-let g:vimsyn_embed='lP'
-
-
-"" Indent Lines config
-let g:indentLine_color_term = 237
-let g:indentLine_char = '▏'
-
 "" Execute macros over visual range
 " visual-at from: https://github.com/stoeffel/.dotfiles/blob/master/vim/visual-at.vim
 xnoremap @ :<C-u>call ExecuteMacroOverVisualRange()<CR>
@@ -79,24 +9,86 @@ function! ExecuteMacroOverVisualRange()
   execute ":'<,'>normal @".nr2char(getchar())
 endfunction
 
-"" Virtualenv python3 provider
-let g:python3_host_prog = $PYENV_ROOT . '/versions/py3nvim/bin/python'
-
-"" Set maximizer default keymaps
-"let g:maximizer_set_default_mapping = 1
-
 "" markdown
 " This interacts with Yggdroot/indentLine plugin: Disable conceal for markdown
 augroup FILETYPES
   autocmd FileType markdown let b:indentLine_enabled = 0
   autocmd FileType markdown set conceallevel=0
 augroup END
-
-"" Fixed python syntax
-let g:python_highlight_all = 1
-
-"" netrw settings
-let g:netrw_banner = 0
-"let g:netrw_liststyle = 3
-"let g:netrw_list_hide = '\(^\|\s\s\)\zs\.\S\+'
 --]]
+
+local set = vim.opt
+
+-- Minimal (but really valuable) settings
+-- Show line numbers
+set.number = true
+-- Enable filetype detection, pluins and indentation
+vim.cmd('filetype plugin indent on')
+-- Always display the status bar
+set.laststatus = 2
+-- Always show the command typed in the status line
+set.showcmd = true
+-- Always show cursor position
+set.ruler = true
+-- Highlight search results
+set.hlsearch = true
+-- Search as you type
+set.incsearch = true
+-- Display command line’s tab complete options as a menu
+set.wildmenu = true
+-- Set screen to scroll following cursor offset
+set.scrolloff = 8
+
+-- Personal choices
+-- Set lines not to wrap
+set.wrap = false
+-- Show line relative numbers instead of usual sequential numbers
+set.relativenumber = true
+-- show existing tab with 4 spaces width
+set.tabstop = 4
+set.softtabstop = -1
+-- when indenting with '>', use 4 spaces width
+set.shiftwidth = 0
+-- On pressing tab, insert 4 spaces
+set.expandtab = true
+-- Smartcase
+set.ignorecase = true
+set.smartcase = true
+
+-- More customization
+-- Highlight current line
+set.cursorline = true
+-- Highlight colums
+set.colorcolumn = { 80, 100 }
+-- Hide native mode indicator
+set.showmode = false
+-- Set update time so vim updates faster
+set.updatetime = 50
+-- Setup persistent undo history
+set.swapfile = false
+set.backup = false
+set.undodir = '~/.vim/undodir'
+set.undofile = true
+set.hidden = true
+-- Hide Pattern not found message from completion
+set.shortmess = vim.o.shortmess .. "c"
+
+-- ====== Other settings ======
+-- Let vim to highlight embeded code: Lua, Python
+vim.g.vimsyn_embed='lP'
+
+-- Indent Lines config
+vim.g.indentLine_color_term = 237
+vim.g.indentLine_char = '▏'
+
+-- Virtualenv python3 provider
+vim.g.python3_host_prog = vim.fn.getenv 'PYENV_ROOT' ..
+  '/versions/py3nvim/bin/python'
+
+-- Fixed python syntax
+vim.g.python_highlight_all = 1
+
+-- netrw settings
+vim.g.netrw_banner = 0
+--vim.g.netrw_liststyle = 3
+--vim.g.netrw_list_hide = '\(^\|\s\s\)\zs\.\S\+'
