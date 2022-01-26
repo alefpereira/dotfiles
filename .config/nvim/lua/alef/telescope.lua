eafbe1583ea141e09abbe0f8e130b7f8ee1c1ac4
Alef Pereira <ealefpereira@gmail.com>
2022-01-26 12:35:15 -0400

    Better display paths in Telescope
    Create filename_path_display function to display
    filename before the path for better visualization.

--- .config/nvim/lua/alef/telescope.lua
+++ .config/nvim/lua/alef/telescope.lua
@@ -11,6 +11,17 @@ end
 local find_command = 'fd --type f --hidden --exclude .git'
 local vimgrep_arguments = 'rg --color=never --no-heading --hidden --with-filename --line-number --column --smart-case --glob !.git'
 
+local utils = require('telescope.utils')
+
+local function filename_path_display(opts, path)
+  local filename = utils.path_tail(path)
+
+  local os_sep = utils.get_separator()
+  local dirname = path:gsub(os_sep .. filename, ' ')
+
+  return string.format('%s - %s', filename, dirname)
+end
+
 -- Telescope.nvim setup
 telescope.setup{
   defaults = {
@@ -32,15 +43,18 @@ telescope.setup{
     find_files = {
       find_command = vim.split(find_command, ' '),
       theme = 'dropdown',
+      path_display = filename_path_display,
     },
     live_grep = {
       vimgrep_arguments = vim.split(vimgrep_arguments, ' '),
       theme = 'dropdown',
+      path_display = filename_path_display,
     },
     buffers = {
-      theme='dropdown',
-      show_all_buffers='true',
-      sort_lastused='true',
+      theme = 'dropdown',
+      show_all_buffers = true,
+      sort_lastused = true,
+      path_display = filename_path_display,
     },
     help_tags = {
       theme='dropdown',
