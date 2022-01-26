93c06dd89451c76b98bbef15dd27d678b676c2d6
Alef Pereira <ealefpereira@gmail.com>
2022-01-26 00:19:40 -0400

    Remove unused rainbow treesitter settings

--- .config/nvim/lua/alef/treesitter.lua
+++ .config/nvim/lua/alef/treesitter.lua
@@ -11,20 +11,4 @@ require'nvim-treesitter.configs'.setup {
   textobjects = {
     enable = true
   },
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    termcolors = {
-      '209',
-      'Green',
-      'Yellow',
-      'Blue',
-      'Magenta',
-      'Cyan',
-      'White',
-    }, -- table of colour name strings
-  },
 }
