5d873e3c062d9e57550fd79aca45caba65c682cd
Alef Pereira <ealefpereira@gmail.com>
2022-02-02 21:31:13 -0400

    Setup null-ls with pylint, eslint and spell

--- .config/nvim/lua/alef/plugins.lua
+++ .config/nvim/lua/alef/plugins.lua
@@ -35,7 +35,13 @@ M.plugins = {
   {
     'jose-elias-alvarez/null-ls.nvim',
     config = function()
-      require("null-ls").setup {
+      local null_ls = require("null-ls")
+      null_ls.setup {
+        sources = {
+          null_ls.builtins.diagnostics.pylint,
+          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.completion.spell,
+        },
       }
     end,
     requires = { 'nvim-lua/plenary.nvim' },
