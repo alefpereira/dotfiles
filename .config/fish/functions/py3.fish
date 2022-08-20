937c7c0d69663a191ac5c11c14ef197d790efbcc
Alef Pereira <ealefpereira@gmail.com>
2022-08-19 23:34:04 -0400

    Fix "ipython3 exists" test in py3 fish function

--- .config/fish/functions/py3.fish
+++ .config/fish/functions/py3.fish
@@ -1,4 +1,4 @@
 # Defined in /home/alef/.config/fish/functions/py3.fish @ line 2
 function py3 --wraps='if ipython3 --version &> /dev/null; ipython3; else; python3; end;' --description 'alias py3=if ipython3 --version &> /dev/null; ipython3; else; python3; end;'
-  if ipython3 --version &> /dev/null; ipython3; else; python3; end; 
+  if command -v ipython3 1> /dev/null; ipython3; else; python3; end;
 end
